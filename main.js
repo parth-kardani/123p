aa1c18ba73aef719ce983bd01541f48dd0c6d554
parth-kardani <77710709+parth-kardani@users.noreply.github.com>
2022-01-30 13:32:02 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+
+function preload(){
+
+}
+
+function setup(){
+canvas=createCanvas(500,500);
+canvas.position(800,150);
+s = 'Hello';
+fill(50);
+textSize(100);
+text(s, 200, 60, 100, 100);
+video=createCapture(VIDEO);
+video.size(500,500);
+}
+
+function draw(){
+
+}
